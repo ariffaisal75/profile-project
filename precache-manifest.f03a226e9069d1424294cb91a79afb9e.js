792e0da794976e3940cd43fcc1d76ebf0efe392e
Irfan Ahmad <irfanibnuahmad@gmail.com>
2020-11-18 22:50:28 +0800

    Updates

---
+++ precache-manifest.f03a226e9069d1424294cb91a79afb9e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fda829001b091b10e51f44291a9c181c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "979ae3bd8c30036b5c47",
+    "url": "/static/css/main.f90d00bd.chunk.css"
+  },
+  {
+    "revision": "97dd5fe0f8151f6d2541",
+    "url": "/static/js/2.55649d38.chunk.js"
+  },
+  {
+    "revision": "0dc713a05373e5c9a88cb999f31ae6e4",
+    "url": "/static/js/2.55649d38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "979ae3bd8c30036b5c47",
+    "url": "/static/js/main.98ebeb1a.chunk.js"
+  },
+  {
+    "revision": "f0a536daecca93e1aeb3",
+    "url": "/static/js/runtime-main.861f3ba5.js"
+  },
+  {
+    "revision": "22f6f24471048d71f8b2aaab50e8ee76",
+    "url": "/static/media/ArifFaisal.22f6f244.pdf"
+  },
+  {
+    "revision": "f4b06b8398f9aa4adfe92c32e1fbf68a",
+    "url": "/static/media/associateCertified.f4b06b83.png"
+  },
+  {
+    "revision": "d377386fcc864e6c1072766f61747c0a",
+    "url": "/static/media/familymart_logo.d377386f.jpg"
+  },
+  {
+    "revision": "c2cf2a7b6b91e5c554936012aa7ec7c2",
+    "url": "/static/media/gmail.c2cf2a7b.svg"
+  },
+  {
+    "revision": "311c2c370ffb5a7e85592de51efa6711",
+    "url": "/static/media/programmer.311c2c37.jpg"
+  },
+  {
+    "revision": "a5892e6b5b078c3cc2ae4a467367c91e",
+    "url": "/static/media/robot.a5892e6b.jpeg"
+  },
+  {
+    "revision": "79ae3a9ae7608e3db72cb84093508aa7",
+    "url": "/static/media/whatsapp.79ae3a9a.svg"
+  }
+]);
